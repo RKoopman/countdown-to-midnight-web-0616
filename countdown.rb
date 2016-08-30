b4d8fc905705ca993887986a01da25ecdfb54ed0
RKoopman <raoul.koopman@gmail.com>
2016-08-30 15:28:47 -0400

    totally overcomplicated that

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
 #write your code here
 
-def countdown
+def countdown(arg)
+  i = 0
+  num = arg + 1
+  while i < arg do
+    i += 1
+      puts "#{num-i} SECOND(S)!"
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(arg)
+  i = 0
+  num = arg + 1
+  while i < arg do
+    i += 1
+    sleep(1)
+      puts "#{num-i} SECOND(S)!"
+  end
+  "HAPPY NEW YEAR!"
 end
